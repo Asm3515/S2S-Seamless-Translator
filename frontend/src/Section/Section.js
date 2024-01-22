bd1e0d40d3085a486bd5287a5dc38f3edaa90e3c
Asm3515 <ajinkya.more3515@gmail.com>
2024-01-22 14:05:56 -0500

    Few more Edits

--- frontend/src/Section/Section.js
+++ frontend/src/Section/Section.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Section.css"
+
 const Section = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    setSelectedFile(file);
+  };
+
   return (
     <div>
-        <section id='profile'>
-            This is Lord RAma
-        </section>
+      <section id='main-app'>
+        <div className='main-app-page'>
+          
+            <img className='Seamless-Picture' src="https://miro.medium.com/v2/resize:fit:1400/1*9T8MUW2WW7wTMmPObVMoNQ.jpeg" alt="META_SEAMLESSV2M4" />
+          
+          <hr />
+          <label className='youtube-input'>
+            Youtube Link : <input name="youtube-link" />
+          </label>
+          <hr />
+          <div
+            className='file-drop-zone'
+            onDrop={handleDrop}
+            onDragOver={(e) => e.preventDefault()}
+          >
+            <label>
+              Drop file here or click to upload
+              <input
+                type="file"
+                name="myRadio"
+                onChange={handleFileChange}
+                style={{ display: 'none' }}
+              />
+            </label>
+            {selectedFile && (
+              <p>Selected file: {selectedFile.name}</p>
+            )}
+          </div>
+          <hr />
+        <div className='output-video'>
+            <p>Please wait until output is produced</p>
+        </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default Section
+export default Section;
